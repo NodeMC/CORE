840dde017d84924550dac0a90c65cabe71143694
md678685 <mathew678@hotmail.co.uk>
2016-05-29 23:06:39 +0100

    Split plugin construction + updated sample
    Split plugin construction into registration, construction and route
    construction, and added perm (for the plugin permission system),
    comments (for plugin comments like v5/plugin API 1.0.0) and target
    (plugin API target, v6 will include 2.0.0) to the sample plugin.

--- plugins/sampleplugin.js
+++ plugins/sampleplugin.js
@@ -3,10 +3,10 @@ class SamplePlugin {
         this.coreEvents = coreEvents;
         this.pluginEvents = pluginEvents;
         this.log = loggerFunction;
-        
+
         loggerFunction("SamplePlugin has been constructed!")
     }
-    
+
     initRoutes(router) {
         // Initialise routes here!
         this.log("SamplePlugin is now (read: would be) initialising routes!")
@@ -15,9 +15,15 @@ class SamplePlugin {
 }
 
 module.exports = {
-    name: "Sample Plugin",
-    id: "space.nodemc.sampleplugin",
-    version: "1.0.0",
-    desc: "Sample Plugin for NodeMC v6",
-    plugin: SamplePlugin
+    name: "Sample Plugin", // Name of the plugin
+    id: "space.nodemc.sampleplugin", // Unique ID for the plugin (formatted like a Java package)
+    version: "1.0.0", // Version of the plugin
+    target: "2.0.0", // NodeMC Plugin API target (which version of the API the plugin is written for)
+    desc: "Sample Plugin for NodeMC (Plugin API 2.0.0)", // Description of the plugin
+    perms: ["route"], // List of permissions which the plugin is requesting
+    comments: [ // Plugin comments
+        "These won't get read by NodeMC.",
+        "You can include instructions for use, credits or whatever you want here."
+    ],
+    plugin: SamplePlugin // The plugin class
 };
