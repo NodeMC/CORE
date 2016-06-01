3de352fb5b5372bc35179c3f62d4a47565486506
md678685 <mathew678@hotmail.co.uk>
2016-06-01 16:21:33 +0100

    Update sample plugin
    Update sample plugin with `apiconf` and semver target

--- plugins/sampleplugin.js
+++ plugins/sampleplugin.js
@@ -18,9 +18,20 @@ module.exports = {
     name: "Sample Plugin", // Name of the plugin
     id: "space.nodemc.sampleplugin", // Unique ID for the plugin (formatted like a Java package)
     version: "1.0.0", // Version of the plugin
-    target: "2.0.0", // NodeMC Plugin API target (which version of the API the plugin is written for)
+    target: "2.0.0", // NodeMC Plugin API compatibility string - see node-semver for valid strings
     desc: "Sample Plugin for NodeMC (Plugin API 2.0.0)", // Description of the plugin
-    perms: ["route"], // List of permissions which the plugin is requesting
+    apiconf: { // Configuration for API access
+        route: { // Configuration for route access
+            enabled: true // Needs to be enabled for plugin to get a express#Router - Default: false
+        },
+        socket: { // Configuration for sockets access
+            enabled: true, // Needs to be enabled for plugin to get a WebSocket namespace - Default: false
+            namespace: "example" // The WebSocket namespace for the plugin - Default: Plugin ID
+        },
+        auth: { // Configuration for the Authentication API
+            enabled: true // Needs to be enabled for plugin to have access to the auth API - Default: false
+        }
+    },
     comments: [ // Plugin comments
         "These won't get read by NodeMC.",
         "You can include instructions for use, credits or whatever you want here."
